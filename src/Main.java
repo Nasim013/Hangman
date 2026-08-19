b38795983d3028d901dd3983ea2558e64fc2f0a7
nulius <nasimmkohov@gmail.com>
2026-08-19 21:26:02 +0300

    Select player(GPT chat helped me)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
+        //Greetings
        System.out.println("Welcome to Hangman game!");
-        System.out.println("Select new game or exit");
+        System.out.println("Select [N]ew game or [E]xit");
+        //Select player
+        Scanner in = new Scanner(System.in);
+        while (true){
+            System.out.print("Input select: ");
+            String select = in.nextLine();
+            if(select.equals("n") || select.equals("N")){
+
+            } else if (select.equals("e") || select.equals("E")){
+                System.out.println("End Hangman game!");
+                break;
+            } else  {
+                System.out.println("Wrong input!");
+            }
+        }
     }
 }
